2e1d4291e3377919c2533eb55f086cb537a2b10a
Andres Puello <andresfpuello16@gmail.com>
2024-01-16 08:51:43 -0400

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './components/About'
 import Certificates from './components/Certificates'
 import Header from './components/Header'
 import Work from './components/Work'
-import Profile from './components/profile'
+import Profile from './components/Profile'
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
